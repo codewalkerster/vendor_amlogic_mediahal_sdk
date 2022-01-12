f37a05feaa1e716d4fc8d8b2265909a124b37c91
yunmin.chen <yunmin.chen@amlogic.com>
2022-01-12 13:17:13 +0800

    tsplayer: CB1 Add setting video crop interface. [1/3]
    PD#SWPL-65090
    
    Problem:
    Add setting video crop interface.
    
    Solution:
    Add setting video crop interface.
    
    Verify:
    SC2
    
    SourceCodec:
    V3.2.176-g15ccc1a
    15ccc1a tsplayer: CB1 Add setting video crop interface. [1/3]
    fd9dc31 tsplayer: CB1 stress test,video freeze [1/1]
    652d33e videodec: CB1 flush all pending pictureready when get last outbuffer [1/
    b8649ad MediaSync: CB1 fix Mediasync Crash issue [1/1]
    8fee7f3 tunnelrender: CB1 add more accurate control for underflow [1/1]
    75bdf1d tunnelmode: CB1 not do frame pull/hold for 50hz [1/1]
    49226e0 videopassthrough: CF1 Support set decoder rp to demux [1/1]
    300097b tsplayer: CF1 demux adds an interface for setting the rp address. [1/1]
    16f3bf8 videodec: CB1 when play some streams the CPU usage is very high. [1/1]
    41aedb1 MediaSync: CB1 fix no audio issue [1/1]
    
    Change-Id: Iac0ba488111da7ad014bc5d48720bee845d05977
    Signed-off-by: yunmin.chen <yunmin.chen@amlogic.com>

--- include/AmTsPlayer.h
+++ include/AmTsPlayer.h
@@ -491,6 +491,7 @@ am_tsplayer_result  AmTsPlayer_getCurrentTime(am_tsplayer_handle Hadl, int64_t *
  *\return:       The AmTsPlayer result.
  */
 am_tsplayer_result  AmTsPlayer_getPts(am_tsplayer_handle Hadl, am_tsplayer_stream_type StrType, uint64_t *pts);
+
 /**
  *\brief:        Set the tsync mode for specified AmTsPlayer instance.
  *\inparam:      AmTsPlayer handle.
@@ -566,6 +567,24 @@ am_tsplayer_result  AmTsPlayer_getBufferStat(am_tsplayer_handle Hadl, am_tsplaye
 am_tsplayer_result  AmTsPlayer_setVideoWindow(am_tsplayer_handle Hadl,
                                                             int32_t x,int32_t y,
                                                             int32_t width,int32_t height);
+
+/*Video interface*/
+/**
+*\brief:        Set the video crop rect size for specified
+*               AmTsPlayer instance.
+*\inparam:      AmTsPlayer handle.
+*\inparam:      The video crop rect left..
+*\inparam:      The video crop rect top..
+*\inparam:      The video crop rect right..
+*\inparam:      The video crop rect bottom..
+*\return:       The AmTsPlayer result.
+*/
+am_tsplayer_result  AmTsPlayer_setVideoCrop(am_tsplayer_handle Hadl,
+                                                      int32_t left,
+                                                      int32_t top,
+                                                      int32_t right,
+                                                      int32_t bottom);
+
 /**
  *\brief:        Set Surface ptr to specified AmTsPlayer instance.
  *\inparam:      AmTsPlayer handle.
@@ -843,12 +862,22 @@ am_tsplayer_result AmTsPlayer_getState(am_tsplayer_handle Hadl,am_tsplayer_state
  *\return:       The AmTsPlayer result.
  */
 am_tsplayer_result  AmTsPlayer_startSub(am_tsplayer_handle Hadl);
+
 /**
  *\brief:        Stop subtitle for specified AmTsPlayer instance .
  *\inparam:      AmTsPlayer handle.
  *\return:       The AmTsPlayer result.
  */
 am_tsplayer_result  AmTsPlayer_stopSub(am_tsplayer_handle Hadl);
+
+/**
+ *\brief:        Get the first pts of specified AmTsPlayer instance.
+ *\inparam:      AmTsPlayer handle.
+ *\inparam:      stream type.
+ *\outparam:     pts.
+ *\return:       The AmTsPlayer result.
+ */
+am_tsplayer_result  AmTsPlayer_getFirstPts(am_tsplayer_handle Hadl, am_tsplayer_stream_type StrType, uint64_t *pts);
 
 #ifdef __cplusplus
 }
