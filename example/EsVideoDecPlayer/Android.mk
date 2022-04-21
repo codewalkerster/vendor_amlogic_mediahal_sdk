87f916e78854d6b1a3e5b8eb40bb83440a2c0e54
Bob Badour <bbadour@google.com>
2022-04-20 21:27:52 -0700

    [LSC] Add LOCAL_LICENSE_KINDS to vendor/amlogic/common
    Added SPDX-license-identifier-GPL SPDX-license-identifier-GPL-2.0
        legacy_unencumbered
    to:
      mediahal_sdk/example/EsVideoDecPlayer/Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Change-Id: Id7dab4e3c71ad4b38cf9a11383a92acca2a81d43
    (cherry picked from commit 996a70d93af6b247207f543776690c6fd93a877c)

--- example/EsVideoDecPlayer/Android.mk
+++ example/EsVideoDecPlayer/Android.mk
@@ -19,5 +19,8 @@ LOCAL_VENDOR_MODULE := true
 
 LOCAL_CFLAGS += -DANDROID_PLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
 
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-GPL SPDX-license-identifier-GPL-2.0 legacy_unencumbered
+LOCAL_LICENSE_CONDITIONS := restricted unencumbered
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../../LICENSE
 include $(BUILD_EXECUTABLE)
 endif
