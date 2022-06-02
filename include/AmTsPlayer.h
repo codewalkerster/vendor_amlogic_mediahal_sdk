dce3146d090a5c6269bf5e623cc2e1e551628b9b
Jianchuan.Ping <jianchuan.ping@amlogic.com>
2022-06-02 13:44:00 +0800

    mediasync: CF2 Add get PCR and STC interface. [1/2]
    PD#SWPL-75800
    
    Problem:
    Need api to get pcr and stc
    
    Solution:
    Add needed interface
    
    Verify:
    AP222
    
    Build Tag:
    Android_Q_DRM_PATCHBUILD-1852
    
    Commits:
    media_hal:
    version: 3.6.19-g7b0d2ab(build:1852)
    7b0d2ab mediasync: CF2 Add get PCR and STC interface. [1/2]
    
    Git Hash-Object:
    b2317fe1f453e64b55f9a2920e2f0e16c7841e6f
    bin/AmTsPlayerExample
    f83f5d7ef55da70afa1fe709eb59f27ad199f7f8
    bin/AmTsPlayerExampleSystem
    c801f067c7ce7580a64bc80fbe5a6d759dd2a2ac
    include/AmTsPlayer.h
    2fc0512f71187a3f53b4f02e08401c92b8a07093
    include/MediaSyncInterface.h
    0c9dfda8466a1a652278c159df1b90858391c750
    lib/system/libmediahal_mediasync.system.so
    6b1677b3aa862f28b3c9b6dd2cf2adb8a361db88
    lib/system/libmediahal_tsplayer.system.so
    d8a9a217ff8a57c2d43f09e6f366ee061e5cedfe
    lib/system/libmediahal_videodec.system.so
    decbd79e4b7c3f6b2e3d72164f69364d1104fb0e
    lib/vendor/libmediahal_mediasync.so
    54ff31e86c91974838e952919b253da5fe1becc7
    lib/vendor/libmediahal_tsplayer.so
    1433501cc741a209a684305429efb2afa332d7b0
    lib/vendor/libmediahal_tunnelrenderer.so
    e4ac230c2369ea0d1c72493dfc0cda1739d2af75
    lib/vendor/libmediahal_videodec.so
    8ed222c85d86d6e4b0873a8fd8768beebed28cff
    version.txt
    
    Change-Id: Iffaf47e77c6a8ed78ddf9976150f3dc246ef6312
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- include/AmTsPlayer.h
+++ include/AmTsPlayer.h
@@ -59,6 +59,7 @@ typedef enum {
     AM_TSPLAYER_KEY_SET_AUDIO_PATCH_MANAGE_MODE,
     AM_TSPLAYER_KEY_AUDIO_SECLEVEL,
     AM_TSPLAYER_KEY_SET_SPDIF_STATUS,
+    AM_TSPLAYER_KEY_SET_OSD,
 } am_tsplayer_parameter;
 
 typedef enum
@@ -149,6 +150,23 @@ typedef enum {
     TS_STREAM_AD = 2,                      // Audio description
     TS_STREAM_SUB = 3,                     // Subtitle
 } am_tsplayer_stream_type;
+
+/*Ts media time type*/
+typedef enum {
+    TS_MEDIA_TIME_VIDEO = 0,                //Video
+    TS_MEDIA_TIME_AUDIO = 1,                //Audio
+    TS_MEDIA_TIME_PCR   = 2,                //PCR
+    TS_MEDIA_TIME_STC   = 3,                //System time clock
+    TS_MEDIA_TIME_MAX,
+} am_tsplayer_media_time_type;
+
+/*Ts time type*/
+typedef enum {
+    TS_UNIT_MS = 0,
+    TS_UNIT_US,
+    TS_UNIT_PTS,
+    TS_UNIT_MAX,
+} am_tsplayer_time_unit;
 
 /*Avsync mode*/
 typedef enum {
@@ -517,6 +535,15 @@ am_tsplayer_result  AmTsPlayer_getCurrentTime(am_tsplayer_handle Hadl, int64_t *
  *\return:       The AmTsPlayer result.
  */
 am_tsplayer_result  AmTsPlayer_getPts(am_tsplayer_handle Hadl, am_tsplayer_stream_type StrType, uint64_t *pts);
+
+/**
+ *\brief:        Get the time of specified AmTsPlayer instance.
+ *\inparam:      AmTsPlayer handle.
+ *\inparam:      stream type.
+ *\outparam:     pts.
+ *\return:       The AmTsPlayer result.
+ */
+am_tsplayer_result  AmTsPlayer_getMediaTime(am_tsplayer_handle Hadl, am_tsplayer_media_time_type mediaTimeType, am_tsplayer_time_unit tunit, uint64_t *time);
 
 /**
  *\brief:        Set the tsync mode for specified AmTsPlayer instance.
@@ -928,6 +955,3 @@ am_tsplayer_result  AmTsPlayer_getFirstPts(am_tsplayer_handle Hadl, am_tsplayer_
 #endif
 
 #endif
-
-
-\
