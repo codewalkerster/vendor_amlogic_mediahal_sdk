312957fcd5a0cffdbbc6ac623f0332b7121fa57f
Yunmin Chen <yunmin.chen@amlogic.com>
2023-10-18 16:47:18 +0800

    hwdumx: CF1 Add the HwDemux module library. [1/1]
    PD#TV-93101
    
    Problem:
    TunnerFramework Inject data speed control.
    
    Solution:
    Add TS stream injection data speed control.
    
    Verify:
    T5M
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-766
    
    Commits:
    media_hal:
    version: 3.22.59-g7d88b00(build:766)
    7d88b00 hwdumx: CB2 stream control not work in vts. [1/1]
    
    vendor/amlogic/common:
    848c8cc AmFFmpegAdapter: CB1 The multimedia_extend has no effect. [3/3]
    
    hardware/amlogic/audio:
    9fac653 audio: Fix XTS tunnel mode issue [1/1]
    
    hardware/amlogic/gralloc:
    dc87662 gralloc: gralloc bingup on kernel 5.4 [1/1]
    
    Git Hash-Object:
    9d3f7eeb3765281c553c804926d3540212d184c8
    Android.bp
    c2d9f76f499148e7862fd2eb2e8dca2e3750c059
    arm64/system/libmediahal_hardware_demux.system.so
    5d30e3afca84008e6ff6f69cb3fdd5f20ed55fef
    arm64/system/libmediahal_mediasync.system.so
    e3512994b06e66f38c12fa96dc9c1b4bc48ee383
    arm64/system/libmediahal_tsplayer.system.so
    fab392c4611c4b4fc4250863b11dfd046df87711
    arm64/system/libmediahal_videodec.system.so
    97fb1805fd9358bfdf2a01c01a767ecd984b1ffb
    arm64/vendor/libmediahal_hardware_demux.so
    13acf4efcb438e338ebd5d99d7cf1ea0cacaa6a5
    arm64/vendor/libmediahal_mediasync.so
    cc5b399ee33246af726690d67c47ea5f583fc836
    arm64/vendor/libmediahal_passthrough.so
    1acf2882f9f3a6f84f73e1a06eb39823bb30556d
    arm64/vendor/libmediahal_tsplayer.so
    9df044ad0804a0200c94132882a7060b73c2d2f5
    arm64/vendor/libmediahal_videodec.so
    f3728689b03918b557edb7981b24b15e7c1918ae
    include/AmHwDemuxInterface.h
    754693e9170b97df8e8b61e43675965247d56e64
    lib/system/libmediahal_hardware_demux.system.so
    0d741558d2a0f8c98a61444da1a23d6e2f8baf42
    lib/system/libmediahal_mediasync.system.so
    6d32788fb7d7eecac90ec265051d31e0dda5ecea
    lib/system/libmediahal_tsplayer.system.so
    4ff3c916efbf0e85bd3a5e0ff0fe096b100cb0b5
    lib/system/libmediahal_videodec.system.so
    75fea3082fe8a8173a1d9ac9c2954ebd2a6c9f5d
    lib/vendor/libmediahal_hardware_demux.so
    886b6e593ad66536a915356060e14eeb24a71832
    lib/vendor/libmediahal_mediasync.so
    cafbc10969578cc816c947832387281ac0562d9f
    lib/vendor/libmediahal_passthrough.so
    4223563950374b61b46f32787e7d95c61a1cea0b
    lib/vendor/libmediahal_tsplayer.so
    adbced2f34f5547b946146e61b6375887519073f
    lib/vendor/libmediahal_videodec.so
    fb07bce7b1be009583847325071926a02bc64002
    version.txt
    
    Change-Id: I372753d5e29da5b5191004a746eb78f149ad72bb
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

---
+++ include/AmHwDemuxInterface.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ */
+
+#ifndef AMHWDEMUX_INTERFACE_H
+#define AMHWDEMUX_INTERFACE_H
+
+
+/*Player working mode*/
+typedef enum {
+   STREAM_CONTROL = 0,
+   GET_ES_MODE = 1,
+} hw_demux_work_mode;
+
+
+/*Function return type*/
+typedef enum {
+    AM_DEMUX_OK  = 0,                      // OK
+    AM_DEMUX_ERROR_INVALID_PARAMS = -1,    // Parameters invalid
+    AM_DEMUX_ERROR_INVALID_OPERATION = -2, // Operation invalid
+    AM_DEMUX_ERROR_INVALID_OBJECT = -3,    // Object invalid
+    AM_DEMUX_ERROR_RETRY = -4,             // Retry
+    AM_DEMUX_ERROR_BUSY = -5,              // Device busy
+    AM_DEMUX_ERROR_END_OF_STREAM = -6,     // End of stream
+    AM_TDEMUX_ERROR_IO            = -7,     // Io error
+    AM_DEMUX_ERROR_WOULD_BLOCK   = -8,     // Blocking error
+    AM_DEMUX_ERROR_MAX = -254
+} am_demux_result;
+
+
+
+struct AmDemuxControlInfo{
+    int demuxId;
+    int mediasyncId;
+    //int aPid;
+    //int vPid;
+};
+
+extern void* AmHwDemux_Create(int mode,void* arg);
+extern void AmHwDemux_Destroy(void* handle);
+extern int AmHwDemux_Init(void* handle,int mode,void* arg);
+extern int AmHwDemux_ResetStatus(void* handle);
+extern int AmHwDemux_SetParams(void* handle,int type, void* arg);
+extern int AmHwDemux_GetParams(void* handle,int type, void* arg);
+extern am_demux_result AmHwDemux_GetStreamControlStatus(void* handle,void* arg,uint64_t WriteTsSize,int vPid,int aPid);
+
+#endif
+
