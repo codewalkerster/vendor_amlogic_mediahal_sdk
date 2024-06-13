905447d26d9e6f0e9f58a0be68adb232a372638b
Qiuye Gan <qiuye.gan@amlogic.com>
2024-06-13 14:03:50 +0800

    mediahal: CB2 no use sdkversion in mediahalvercheck_u.go.in [1/1]
    PD#SWPL-172161
    
    Problem:
    no use sdkversion in mediahalvercheck_u.go.in for androidU
    
    Solution:
    remove the code of sdkversion in mediahalvercheck_u.go.in
    
    Verify:
    ohm
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-3742
    
    Commits:
    media_hal:
    version: 3.28.11-gcc0e5d8(build:3742)
    cc0e5d8 mediahal: CB2 no use sdkversion in mediahalvercheck_u.go.in [1/1]
    
    vendor/amlogic/common:
    7bd60093 vdec: add s6 signed ta. [1/1]
    
    hardware/amlogic/audio:
    727e833 audio: t7c vts fail [1/1]
    
    hardware/amlogic/gralloc:
    ffaec2d gralloc: fix compile error [1/1]
    
    Git Hash-Object:
    b83a862f592248605387846ad3fe216e9a575c3e
    arm64/system/libmediahal_resman.system.so
    2ce83fac648ac962cd6847797d0f9775814f83c9
    arm64/system/libmediahal_tsplayer.system.so
    d519576b26bf9a84fdac2ccba3779237ed00fe27
    arm64/system/libmediahal_videodec.system.so
    e4f63b5f42ce00935da4261f299ef3464567558b
    arm64/vendor/libmediahal_passthrough.so
    4f6eabfbf363055128f3df4cd61995df444df23a
    arm64/vendor/libmediahal_resman.so
    c4282f1dd23b045967effb37c7259a659d45aee7
    arm64/vendor/libmediahal_tsplayer.so
    0a62a55cbb5617b07dee01b7b5b124d024aabfa8
    arm64/vendor/libmediahal_tunnelrenderer.so
    5639d250a63f5fae869617d2d1de6aef5f023145
    arm64/vendor/libmediahal_videodec.so
    8a1589b2afbec7207eebb8d9cb6e3c15cf99382b
    lib/system/libmediahal_resman.system.so
    9e6276ad2b8b4070d32b04998041e88a9a436719
    lib/system/libmediahal_tsplayer.system.so
    8e6c51a1e56610556d41633b7179e5a3eb508f99
    lib/system/libmediahal_videodec.system.so
    c034c17a79a307d4d06992c0a23e75a2979304df
    lib/vendor/libmediahal_passthrough.so
    353b8dfb4a8d623a070d37dd189ca526a8aaf3e3
    lib/vendor/libmediahal_resman.so
    9609da0e2efc8a8117af70aaa992002284c63492
    lib/vendor/libmediahal_tsplayer.so
    dc6ccdc734d5b6e423e925e1ad38fd144524bbf8
    lib/vendor/libmediahal_tunnelrenderer.so
    eb607ac6ef251931585bb0af88603c7e156b3194
    lib/vendor/libmediahal_videodec.so
    fc972e0bd93efbdabd4a713ae62d04d720c52156
    mediahalvercheck.go
    844bd316a5f2f443964984fdeae2fef0c280e1ef
    version.txt
    
    Change-Id: Iff775799005ab70ee4e365217b703675ea346d33
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- mediahalvercheck.go
+++ mediahalvercheck.go
@@ -5,44 +5,23 @@ import (
     "android/soong/cc"
     //"fmt"
     "github.com/google/blueprint/proptools"
-    "strconv"
 )
 
 func MediahalVerCheckDefaults(ctx android.LoadHookContext) {
-    sdkVersion := ctx.Config().PlatformSdkVersion().String()
-    sdkVersionInt,err := strconv.Atoi(sdkVersion)
-    if err != nil {
-        //fmt.Printf("%v fail to convert", sdkVersionInt)
-    } else {
-        //fmt.Println("CheckDefaults sdkVersion:", sdkVersionInt)
+    type props struct {
+        System_ext_specific  *bool
     }
-    if sdkVersionInt >= 30 {
-        type props struct {
-            System_ext_specific  *bool
-        }
-        p := &props{}
-        p.System_ext_specific = proptools.BoolPtr(true)
-        ctx.AppendProperties(p)
-    }
+    p := &props{}
+    p.System_ext_specific = proptools.BoolPtr(true)
+    ctx.AppendProperties(p)
 }
 
 func MediahalVerCheckMediahalPassthroughDefaults(ctx android.LoadHookContext) {
     type props struct {
-    Enabled  *bool
+        Enabled  *bool
     }
     p := &props{}
-    // Enabled by default, when sdkVersion >= 30 or sdkVersion is not a valid number string
     p.Enabled = proptools.BoolPtr(true)
-    sdkVersion := ctx.Config().PlatformSdkVersion().String()
-    sdkVersionInt,err := strconv.Atoi(sdkVersion)
-    if err != nil {
-        //fmt.Printf("%v fail to convert", sdkVersionInt)
-    } else {
-        //fmt.Println("PassthroughDefaults sdkVersion:", sdkVersionInt)
-    if sdkVersionInt < 30 {
-        p.Enabled = proptools.BoolPtr(false)
-    }
-    }
     ctx.AppendProperties(p)
 }
 
