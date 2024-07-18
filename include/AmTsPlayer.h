55bf49ccb15eea13e41c09c7a01f939076225907
Qiuye Gan <qiuye.gan@amlogic.com>
2024-07-18 17:53:31 +0800

    tsplayer: CF1 support h266 decoder [1/1]
    PD#SWPL-175670
    
    Problem:
    tsplayer supports h266 decoder
    
    Solution:
    tsplayer supports h266 decoder
    
    Verify:
    raman
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-4341
    
    Commits:
    media_hal:
    version: 3.28.33-g3344631(build:4341)
    3344631 tsplayer: CF1 support h266 decoder [1/1]
    
    vendor/amlogic/common:
    20d509f2 DLG: 2160p60hz is not diff group with 3840x2160p120hz [1/1]
    
    hardware/amlogic/audio:
    124a285e audio: fix Virtualsurround crash [1/1]
    
    hardware/amlogic/gralloc:
    b4004e1 gralloc: disable rgb888 for utgard [1/1]
    
    Git Hash-Object:
    2cf3479c15c2fa572dda822ccfca99b39a8af86a
    arm64/bin/AmTsPlayerExample
    dbd6a59f9fc0885214d16028d5e73eb3a3b12f98
    arm64/bin/AmTsPlayerExampleSystem
    acb9f9a7267483a4804e7d370d4854f4f6912e59
    arm64/system/libmediahal_tsplayer.system.so
    842d2a6f4b01bfe02fc6aec14afd22165bc04ed0
    arm64/system/libmediahal_videodec.system.so
    7c8561c1bf98dc87d0916bcce46ab69b94e0c119
    arm64/vendor/libmediahal_passthrough.so
    0acfdc0c66feeecefa1023409e3a0bac5b26db3e
    arm64/vendor/libmediahal_tsplayer.so
    5b0e19513275104f8ba65b7ac52be7172fe4a4a2
    arm64/vendor/libmediahal_videodec.so
    da21efa1e48a40688b30d2626623da3f5d0d6bb6
    bin/AmTsPlayerExample
    d51e0f18094de772142d69f484d7fe301ba4c30a
    bin/AmTsPlayerExampleSystem
    b4a69f47fb1f4dba8f9a1a626df42f3f597a3b80
    bin/AmTsPlayerTest
    356442f1f67d1a9516daacc768cec1a4499bb5ec
    example/AmTsPlayerExample/AmTsPlayerExample.cpp
    15069fcd3a269d5b1a8784fff4fcd951ea3eb627
    include/AmTsPlayer.h
    e2ae632c6dc84489372f06453354b695b1da3218
    lib/system/libmediahal_tsplayer.system.so
    1b2be3b9ea5a63547400b1a1cf6602f57bef5c2b
    lib/system/libmediahal_videodec.system.so
    a6bbd185070d806dc2461f4603817937aab75c57
    lib/vendor/libmediahal_passthrough.so
    d6de31631faed6e5ea390fb54c3f00b365f05692
    lib/vendor/libmediahal_tsplayer.so
    c89c0574d18180471397733f08bacf1b8075db97
    lib/vendor/libmediahal_videodec.so
    d702a5d0ebf88fed88a247161b8c58f95b42cd65
    version.txt
    
    Change-Id: I50987df5d0dc20f68fdd9918dbf9fb1c58081a4a
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- include/AmTsPlayer.h
+++ include/AmTsPlayer.h
@@ -296,6 +296,7 @@ typedef enum {
     AV_VIDEO_CODEC_DVES_HEVC = 11,         // DVES_HEVC
     AV_VIDEO_CODEC_AVS3 = 12,              // AVS3
     AV_VIDEO_CODEC_AV1 = 13,              // AV1 (Only support on frame mode)
+    AV_VIDEO_CODEC_H266 = 14,               // H266
     AV_VIDEO_CODEC_MAX = 1000,             // Out of range type (Unsupport)
 } am_tsplayer_video_codec;
 
