6dccc3bffc212e40196416e89d45a71a5a1aac61
Dehong Chen <dehong.chen@amlogic.com>
2025-04-27 10:36:26 +0800

    tsplayer: CF2 add I only trickmode bypass mediasync mode [1/1]
    PD#OTT-68714
    PD#SWPL-185127
    
    Problem:
    need add I only trickmode bypass mediasync mode.
    
    Solution:
    need add I only trickmode bypass mediasync mode.
    
    Verify:
    ohm
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-8988
    
    Commits:
    media_hal:
    version: 3.32.72-gf47f57ff(build:8988)
    f47f57ff tsplayer: CF2 add I only trickmode bypass mediasync mode [1/1]
    
    vendor/amlogic/common:
    1c58b49a remote: support a new key [2/3]
    
    hardware/amlogic/audio:
    b2120d4f audio : use rt thread to mlock hal so [2/2]
    
    hardware/amlogic/gralloc:
    6b7aeb4 GPU: add NV12 format permitted usage [1/1]
    
    Git Hash-Object:
    c9c0324816547298394d8287bb483c4959a66d92
    arm64/bin/AmTsPlayerExample
    cf7ad37c6b062e2f272c2dc347cfe1cf48e4eed4
    arm64/bin/AmTsPlayerExampleSystem
    66683e46ebbb9d5f2bf6a11e696b4e69a6efbf21
    arm64/system/libmediahal_tsplayer.system.so
    c5a87e9f372c42736e71b60c079a7c9a066a841f
    arm64/system/libmediahal_videodec.system.so
    e0ca0a29007e0eba7e397afdbd2b5294cf43cbf2
    arm64/vendor/libmediahal_passthrough.so
    25e2a4396b747e2e4f77f7f5370aa24672d467d8
    arm64/vendor/libmediahal_tsplayer.so
    ae156b204dc412c57f5244dbbe90afaae3a343b7
    arm64/vendor/libmediahal_tunnelrenderer.so
    afaaf635835dcbe547fe25f9c2799eaeea86cdce
    arm64/vendor/libmediahal_videodec.so
    b57a3184ea6766e8c5d477068875b388de8f0f58
    bin/AmTsPlayerExample
    1ef3d3790e6b71c9f62dbf66dc611b4aa48dd92a
    bin/AmTsPlayerExampleSystem
    c41500fdb9909c0c3809325dba8d5b181d4fd765
    bin/AmTsPlayerTest
    6b8899044ed27c6472f87c8e5f8c2684b0a94184
    include/AmTsPlayer.h
    64932694558bd1bd90e05b63a254f2952ebbc395
    lib/system/libmediahal_tsplayer.system.so
    87679bb4cbee2bebc1e4d988013ad138ed9615aa
    lib/system/libmediahal_videodec.system.so
    efa1ab8442bec7cd7cffb0ca1b91b951064148de
    lib/vendor/libmediahal_passthrough.so
    e4ca52184084bb306bf3774dc4e6524092420045
    lib/vendor/libmediahal_tsplayer.so
    7c008e780dc81506f273be5e93032d5f91937008
    lib/vendor/libmediahal_tunnelrenderer.so
    833695a7889243c3dda6679a887fbd651be366bb
    lib/vendor/libmediahal_videodec.so
    672d45d59217eca8c7dee799b7e96c4b9c32c066
    version.txt
    
    Change-Id: I257026bc7a1e00de29b3504fed9bbe613cbad5ce
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- include/AmTsPlayer.h
+++ include/AmTsPlayer.h
@@ -55,6 +55,7 @@ typedef enum {
     AM_TSPLAYER_EVENT_TYPE_DECODER_INIT_COMPLETED, //Decoder init completed
     AM_TSPLAYER_EVENT_TYPE_DEMUX_FIRST_PTS, //demux first pts
     AM_TSPLAYER_EVENT_TYPE_AUDIO_FORMAT_CHANGED, // Audio format changed
+    AM_TSPLAYER_EVENT_TYPE_FFFB_VIDEO_TIMESTAMP, //FF/FB Current video timestamp
 } am_tsplayer_event_type;
 
 
@@ -270,7 +271,8 @@ typedef enum {
     AV_VIDEO_TRICK_MODE_NONE = 0,          // Disable trick mode
     AV_VIDEO_TRICK_MODE_PAUSE = 1,         // Pause the video decoder
     AV_VIDEO_TRICK_MODE_PAUSE_NEXT = 2,    // Pause the video decoder when a new frame displayed
-    AV_VIDEO_TRICK_MODE_IONLY = 3          // Decode and out I frame only
+    AV_VIDEO_TRICK_MODE_IONLY = 3,         // Decode and out I frame only
+    AV_VIDEO_TRICK_MODE_IONLY_AVSYNC_BYPASS = 4  // Decoding and Out I frame only, bypass mediasync to playback
 } am_tsplayer_video_trick_mode;
 
 /*Video display match mode*/
