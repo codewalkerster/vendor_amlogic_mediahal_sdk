0238aae887c9bad6bec9117950d40a2958e300f2
Miaohong Chen <miaohong.chen@amlogic.com>
2025-07-30 16:47:03 +0800

    prebuilt: update prebuilts from Android_U_DRM_PATCHBUILD-10679 [1/1]
    PD#NONE
    
    Problem:
    Update prebuilt files
    
    Solution:
    None
    
    Verify:
    None
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-10679
    
    Commits:
    media_hal:
    version: 3.32.82-gab236c66-p1(build:10679)
    ab236c66 dhp: CF1 DHP_DAEMON directory migration. [1/3]
    b3b67cc7 videodec: CB0 fix occasional crash issue when playing lcevc [1/1]
    
    vendor/amlogic/common:
    11c50f4a0 overlay: add AtvAxelOverlay [1/1]
    
    hardware/amlogic/audio:
    485aa40c audio_hal: fix hdmi-out and bt all output sound issue [1/1]
    
    hardware/amlogic/gralloc:
    8975142 gralloc: gralloc enable format 0x2b [1/1]
    
    Git Hash-Object:
    9aa96a07d95a1f5cd71b2a2cbe3403bb10c7f882
    Android.bp
    22108f8655186c201795ca9f2ec81ec2097b7741
    arm64/system/libmediahal_tsplayer.system.so
    e763f6e91877ca962870d493ffdaffa6d62b766a
    arm64/system/libmediahal_videodec.system.so
    af3d2a22ff2784c5d0e6805d6f8a9214be18c537
    arm64/vendor/libavbc_soft_decoder.so
    dc5bee74b98d5caca301c7541375d167a55721ac
    arm64/vendor/libmediahal_passthrough.so
    2c0ea69c369d4c95b49ced0310b571056a10ca6e
    arm64/vendor/libmediahal_tsplayer.so
    71913f348f6c493640fcd0154d22847e3f3b7080
    arm64/vendor/libmediahal_videodec.so
    0d8f1ca2199c3e86dd2255af41c108fbbf95ca62
    bin/dhp_daemon
    4924c9cad53dc7c7c65e48534342667116f8dc18
    init.dhp_daemon.rc
    d929db98296da4509933c96f5ee4fa52376e359d
    lib/system/libmediahal_tsplayer.system.so
    b63662497e500ae1d228d378c13c2d2685055645
    lib/system/libmediahal_videodec.system.so
    af3d2a22ff2784c5d0e6805d6f8a9214be18c537
    lib/vendor/libavbc_soft_decoder.so
    4e76755dcb182e12de4450cf6425815e0bb95b23
    lib/vendor/libmediahal_passthrough.so
    148f9f203975dff95cc41701e06474ee43a43887
    lib/vendor/libmediahal_tsplayer.so
    97cd40462966e1b6311f7f1e86fe3138f1665ae2
    lib/vendor/libmediahal_videodec.so
    c1e714329b70bce5517497622477351579b041be
    mediahal_sdk.go
    e3bdb280d9a62a7fd9dccc7a0b3cddf00fe6657c
    version.txt
    
    Change-Id: Ic9363a91c873cb9f841bd886aed090f2bb0364c6
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- mediahal_sdk.go
+++ mediahal_sdk.go
@@ -9,6 +9,7 @@ import (
 
 func init() {
     android.RegisterModuleType("mediahal_sdk_go_defaults", mediahal_sdk_go_DefaultsFactory)
+    android.RegisterModuleType("dhp_sdk_go_defaults", dhp_sdk_go_DefaultsFactory)
 }
 
 func mediahal_sdk_go_DefaultsFactory() (android.Module) {
@@ -38,3 +39,31 @@ func mediahal_sdk_go_DefaultsFactory() (android.Module) {
     })
     return module
 }
+
+func dhp_sdk_go_DefaultsFactory() (android.Module) {
+    module := cc.DefaultsFactory()
+    android.AddLoadHook(module, func(ctx android.LoadHookContext) {
+        type props struct {
+            Enabled *bool
+        }
+        p := &props{}
+
+        pdhpSrcPath := "hardware/amlogic/media_modules_5.15/drivers/frame_provider/aml_dhp/dhp_daemon"
+        tdhpSrcPath := "hardware/amlogic/media_modules/drivers/frame_provider/aml_dhp/dhp_daemon"
+        udhpSrcPath := "common/common14-5.15/driver_modules/media_modules/drivers/frame_provider/aml_dhp/dhp_daemon"
+        bdhpSrcPath := "common/common16-6.12/driver_modules/media_modules/drivers/frame_provider/aml_dhp/dhp_daemon"
+        if android.ExistentPathForSource(ctx, pdhpSrcPath).Valid() == true {
+            p.Enabled = proptools.BoolPtr(false)
+        } else if android.ExistentPathForSource(ctx, tdhpSrcPath).Valid() == true {
+            p.Enabled = proptools.BoolPtr(false)
+        } else if android.ExistentPathForSource(ctx, udhpSrcPath).Valid() == true {
+            p.Enabled = proptools.BoolPtr(false)
+        } else if android.ExistentPathForSource(ctx, bdhpSrcPath).Valid() == true {
+            p.Enabled = proptools.BoolPtr(false)
+        } else {
+        }
+        ctx.AppendProperties(p)
+    })
+    return module
+}
+
